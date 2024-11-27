0a2bada33384a78cddc06724f61aca15b2c22094
Sanjeev Kumar <ksanjeev284@gmail.com>
2024-11-27 14:37:51 +0530

    Fix base path in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site: 'https://ksanjeev284.github.io',
-  base: '/',
+  base: '/ksanjeev284.github.io',
   output: 'static',
 });
