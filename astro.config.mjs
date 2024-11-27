1e38ea118d21516147ca2fddbf4a736fd762cc85
Sanjeev Kumar <ksanjeev284@gmail.com>
2024-11-27 14:40:32 +0530

    Update deployment configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site: 'https://ksanjeev284.github.io',
-  base: '/ksanjeev284.github.io',
+  base: '/',
   output: 'static',
+  build: {
+    assets: '_astro'
+  }
 });
